c4eb842ee9e153009f15c10a330db3a176b6254c
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-01-26 18:08:27 +0530

    added function to store deal

--- hardhat/contracts/Store.sol
+++ hardhat/contracts/Store.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
+
+import {FilecoinMarketConsumer} from "./filecoin-api/FilecoinMarketConsumer.sol";
+import {MarketTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
 
 contract Store {
     uint256 public id;
@@ -14,12 +17,35 @@ contract Store {
         string supportable;
         bool _private;
         address owner;
+        uint fundingPeriod;
     }
+
+    /**
+     * @notice this struct is used to store the data of the deal on-chain
+     * @dev after the deal is created this struct will have all the related info of that deal
+     */
+    struct dealData {
+        string label;
+        uint64 dealClientActorId;
+        uint64 dealProviderActorId;
+        bool isDealActivated;
+        bytes commitment;
+        uint64 size;
+        int64 startTerm;
+        int64 endTerm;
+        int64 activated;
+        int64 terminated;
+
+    }
+
     // to get to frontend
     metadata[] public data;
 
+    FilecoinMarketConsumer market = FilecoinMarketConsumer(0xBbb4a0CC1b52EeaAfb52a5F3E15892837e20A4e2);
+
     //may be will remove this in future
     mapping(uint256 => metadata) public dataById;
+    mapping(uint256 => dealData) public dealDataById;
     mapping(address => metadata[]) public ownerData;
     mapping(uint256 => address[]) public supportersOfDataId;
 
@@ -29,7 +55,8 @@ contract Store {
         string memory _cid,
         string memory _supportable,
         bool _private,
-        uint256 _price
+        uint256 _price,
+        uint _fundingPeriod
     ) external {
         data.push(
             metadata(
@@ -40,7 +67,8 @@ contract Store {
                 _cid,
                 _supportable,
                 _private,
-                msg.sender
+                msg.sender,
+                _fundingPeriod
             )
         );
         dataById[id] = data[id];
@@ -61,4 +89,51 @@ contract Store {
         //need to add the payment details here
         supportersOfDataId[_id].push(msg.sender);
     }
+
+    /**
+     * @dev this function is called by bounty hunter after uploading data on filecoin
+     * This function registers the deal data based on the dealID provided by bounty hunter
+     * @param _id of the data
+     * @param dealId to get the deal data from filecoin and store it in smart contract
+     */
+    function registerDeal(uint _id, uint64 dealId) public{
+        require(_id < id, "Wrong data id!"); 
+        require(dataById[_id].fundingPeriod < block.timestamp, "Funding period is not over yet!");
+        market.storeAll(dealId);
+
+        (bytes memory commitment, uint64 size) = market.dealCommitment();
+        (int64 startTerm, int64 endTerm) = market.dealTerm();
+        (int64 activated, int64 terminated) = market.activationStatus();
+
+        dealData memory newDeal = dealData(
+            market.dealLabel(),
+            market.dealClientActorId(),
+            market.dealProviderActorId(),
+            market.isDealActivated(),
+            commitment,
+            size,
+            startTerm,
+            endTerm,
+            activated,
+            terminated
+            );
+
+        dealDataById[_id] = newDeal;
+
+    }
+
+    /**
+     * @dev this function is called by bounty hunter to claim his bounty
+     * @dev this function should be called after calling registerDeal function
+     * @param _id of the data
+     * @param dealId to verify the data has been uploaded or not on-chain 
+     */
+    function verifyClaim(uint _id, uint dealId) public view returns (bool){
+        //need to verify the claim here
+        return true;
+    }
+
+
+    fallback() external payable {}
+    receive() external payable {}
 }
