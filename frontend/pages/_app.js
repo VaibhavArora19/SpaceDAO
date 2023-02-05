c26efc1d4fb6ea9b71fcd6c26b9702f972dbf6e7
Dinesh <dineshaitham2@gmail.com>
2023-02-05 21:53:56 +0530

    added wallet connect

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,17 +2,66 @@ import Layout from "@/components/Layout";
 import AppWrapper from "../context/DataContext";
 import "@/styles/globals.css";
 import { WalletProvider } from "@/context/WalletContext";
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+const hyperspace = {
+  id: 3141,
+  name: "Filecoin - Hyperspace testnet",
+  network: "Filecoin - Hyperspace testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "FILECOIN",
+    symbol: "tFIL",
+  },
+  rpcUrls: {
+    default: { http: ["https://api.hyperspace.node.glif.io/rpc/v1"] },
+  },
+  blockExplorers: {
+    default: { name: "Hyperspace", url: "https://hyperspace.filfox.info/en" },
+  },
+};
+
+const chains = [hyperspace];
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "18eaa862738a7e299d4dd9cd925f90b3" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    appName: "SpaceDAO",
+    chains,
+  }),
+  provider,
+});
+
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <div className="">
-        <AppWrapper>
-          <WalletProvider>
-            <Component {...pageProps} />
-          </WalletProvider>
-        </AppWrapper>
-      </div>
-    </Layout>
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Layout>
+          <div className="">
+            <AppWrapper>
+              <WalletProvider>
+                <Component {...pageProps} />
+              </WalletProvider>
+            </AppWrapper>
+          </div>
+        </Layout>
+      </WagmiConfig>
+      <Web3Modal
+        projectId="18eaa862738a7e299d4dd9cd925f90b3"
+        ethereumClient={ethereumClient}
+      />
+    </>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,9 +5,12 @@ import WalletModal from "@/components/WalletModal";
 import { WalletContext } from "@/context/WalletContext";
 import Head from "next/head";
 import { useContext } from "react";
+import { useAccount } from "wagmi";
 
 export default function Home() {
   const { currentAccount } = useContext(WalletContext);
+
+  const { address, isConnecting, isDisconnected } = useAccount();
 
   return (
     <>
@@ -17,9 +20,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {!address && <WalletModal />}
       <main>
-        {!currentAccount && <WalletModal />}
-
         <HeroCard />
         <Games />
         <Books />
